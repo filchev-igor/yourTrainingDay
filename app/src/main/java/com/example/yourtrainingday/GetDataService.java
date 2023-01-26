663edf0c2981ec9e738c4ae2ed83c1ec743a0417
Igor Filchev <filchev.igor@gmail.com>
2023-01-26 13:46:36 +0200

    Create login fragment

---
+++ app/src/main/java/com/example/yourtrainingday/GetDataService.java
@@ -0,0 +1,20 @@
+package com.example.yourtrainingday;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface GetDataService {
+    @FormUrlEncoded
+    @POST("/realms/Android-demo/protocol/openid-connect/token")
+    Call<AccessToken> getAccessToken(
+      @Field("client_id") String client_id,
+      @Field("grant_type") String grant_type,
+      @Field("client_secret") String client_secret,
+      @Field("scope") String scope,
+      @Field("username") String username,
+      @Field("password") String password
+
+    );
+}
