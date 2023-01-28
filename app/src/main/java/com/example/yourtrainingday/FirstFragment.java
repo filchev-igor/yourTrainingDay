0e4a2d1e3da513689d47e55944caff4f10289aba
Igor Filchev <filchev.igor@gmail.com>
2023-01-28 07:35:24 +0200

    Corrections after rebase

--- app/src/main/java/com/example/yourtrainingday/FirstFragment.java
+++ app/src/main/java/com/example/yourtrainingday/FirstFragment.java
@@ -25,10 +25,6 @@ public class FirstFragment extends Fragment {
 
     }
 
-
-
-
-
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
--- app/src/main/java/com/example/yourtrainingday/UserPageFragment.java
+++ app/src/main/java/com/example/yourtrainingday/UserPageFragment.java
@@ -10,53 +10,41 @@ import androidx.fragment.app.Fragment;
 
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.yourtrainingday.databinding.FragmentSecondBinding;
+import com.example.yourtrainingday.databinding.FragmentUserPageBinding;
 
-public class UserPage extends Fragment {
-
-    private FragmentUserPage binding;
+public class UserPageFragment extends Fragment {
+    private FragmentUserPageBinding binding;
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = UserPageBinding.inflate(inflater, container, false);
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        binding = FragmentUserPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonPlanai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(UserPage.this)
-                        .navigate(R.id.action_userPageFragment_to_planaiFragment);
-            }
-            binding.buttonTreniruotes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    NavHostFragment.findNavController(UserPage.this)
+        binding.buttonPlanai.setOnClickListener(view12 -> {
+            NavHostFragment.findNavController(UserPageFragment.this)
                             .navigate(R.id.action_userPageFragment_to_planaiFragment);
-                }
-                binding.buttonPratimai.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        NavHostFragment.findNavController(UserPage.this)
-                                .navigate(R.id.action_userPageFragment_to_planaiFragment);
-                    }
-            binding.buttonTreneriai.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            NavHostFragment.findNavController(UserPage.this)
-                                    .navigate(R.id.action_userPageFragment_to_planaiFragment);
-                        }
         });
 
+        binding.buttonTreniruotes.setOnClickListener(view1 -> {
+            NavHostFragment.findNavController(UserPageFragment.this)
+                    .navigate(R.id.action_userPageFragment_to_planaiFragment);
+        });
 
+        binding.buttonPratimai.setOnClickListener(view13 -> {
+            NavHostFragment.findNavController(UserPageFragment.this)
+                .navigate(R.id.action_userPageFragment_to_planaiFragment);
+        });
+
+        binding.buttonTreneriai.setOnClickListener(view14 -> {
+            NavHostFragment.findNavController(UserPageFragment.this)
+                .navigate(R.id.action_userPageFragment_to_planaiFragment);
+        });
     }
 
     @Override
@@ -64,6 +52,4 @@ public class UserPage extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
-        }
